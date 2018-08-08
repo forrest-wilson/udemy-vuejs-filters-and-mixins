54a8a45b1e9453cadacfc53a348697c06d53c571
Forrest Wilson <forrest.wilson@me.com>
2018-08-09 11:18:58 +1200

    Global mixin - use with caution

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 
 Vue.filter('to-lowercase', val => { return val.toLowerCase() });
 
+Vue.mixin({
+  created () {
+    console.log('Global mixin - created hook');
+  }
+});
+
 new Vue({
   el: '#app',
   render: h => h(App)
